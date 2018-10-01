b97d6764d108b7576a2c24bdc88c994a6f0c4c49
Hidayat arghandabi <hidayatarg@gmail.com>
2018-10-01 11:06:37 +0300

    Capitalize each title first word

--- textUtilities.js
+++ textUtilities.js
@@ -4,10 +4,21 @@ var expect= require('chai').expect;
 // Writing expections
 expect(true).to.be.true;
 
+function titleCase(title) {
 
-expect(titleCase('the great mouse detective')).to.be.a('string');
+   var words = title.split(" ");
+   var titleCasedWords = words.map(function (word) {
+       return word[0].toUpperCase() + word.substring(1);
+   });
+   return titleCasedWords.join(" ");
 
-function titleCase(title){
+   }
+   
 
-    return title;
-}
+
+
+expect(titleCase("the great mouse detective")).to.be.a("string");
+expect(titleCase('a')).to.equal("A");
+expect(titleCase('vertigo')).to.equal('Vertigo'); 
+expect(titleCase("the great mouse detective")).to.equal("The Great Mouse Detective");
+
